bc2a222a5d2df941b78b7ce81114b00304964541
raedownen <rae.l.downen@gmail.com>
2023-03-16 22:38:59 -0500

    another commit

--- code.js
+++ code.js
@@ -4,20 +4,39 @@ function helloWorld() {
 }
 //sayHello function
 function sayHello(name) {
-    if(name === undefined) {
-        return "Hello"
+    if(name === undefined || name === null) {
+        return "Hello, World!"
     }
-    if (name === "Jane") {
+    if(name === "Jane") {
         return "Hello, Jane"
     } else if (name === "Alex") {
         return "Hello, Alex"
-    } else if (name === "Pat") {
+    } else if  (name === "Pat") {
         return "Hello, Pat"
     } else if (name === true || name === false){
         return "Hello, World!"
     }
     else {
         return ("Hello," + name + "!");
+    }
+}
+
+//isFive function
+function isFive(input) {
+    if (input === 5 || input === '5') {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+//isEven function
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true
+    } else {
+        return false
     }
 }
 
--- tests.js
+++ tests.js
@@ -40,6 +40,9 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat" when executed', function() {
         expect(sayHello ("Pat")).toBe("Hello, Pat");
     });
+    it('should return the string "Hello, World!" when excuted', function() {
+        expect(sayHello()).toBe("Hello, World!")
+});
     it('should return the string "Hello, World!" when exceuted', function() {
         expect(sayHello(true)).toBe("Hello, World!")
     });
@@ -48,56 +51,53 @@ describe('sayHello', function() {
     });
 });
 
-//New test for sayHello function
+//New test for isFive function
 
-// //Unit test for sayHello function
-// describe('sayHello', function() {
-//     it('should be a defined function', function() {
-//         expect(typeof sayHello).toBe('function');
-//     });
-//     it('should return a string when called', function() {
-//         expect(typeof sayHello()).toBe('string');
-//     });
-//     it("should return the string '' when executed", function() {
-//         expect(sayHello()).toBe('');
-//     });
-//     it("should never return 'undefined' when called", function() {
-//         expect(typeof sayHello()).not.toBe(undefined);
-//     });
-// });
-//
-// //Unit Test for SayHello(name) function
-//
-// describe('sayHello', function() {
-//     it('should be a defined function', function() {
-//         expect(typeof sayHello).toBe('function');
-//     });
-//     it('should return a string when called', function() {
-//         expect(typeof sayHello()).toBe('string');
-//     });
-//     it("should return the string 'Hello, Jane' when executed", function() {
-//         expect(sayHello('Jane')).toBe('Hello, Jane');
-//     });
-//     it("should never return 'undefined' when called", function() {
-//         expect(typeof sayHello(name)).not.toBe(undefined);
-//     });
-// });
+describe('isFive', function() {
+    it('should be a defined function', function() {
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return true when 5 is passed', function() {
+        expect(isFive(5)).toBe(true);
+    });
+    it("should return true when the string of '5' is passed", function() {
+        expect(isFive("5")).toBe(true);
+    });
+});
 
-//Say helloAlex function
+//isEven function
 
-// describe('sayHello', function() {
-//     it('should be a defined function', function() {
-//         expect(typeof sayHello).toBe('function');
-//     });
-//     it('should return a string when called', function() {
-//         expect(typeof sayHello()).toBe('string');
-//     });
-//     it("should return the string '' when executed", function() {
-//         expect(sayHelloAlex('Alex')).toBe('Alex');
-//     });
-//     it("should never return 'undefined' when called", function() {
-//         expect(typeof sayHello3('Alex')).not.toBe(undefined);
-//     });
-// });
-//
-//
+describe('isEven', function() {
+    it('should be a defined function', function() {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return a boolean value when passed any number', function() {
+        expect(typeof isEven(Number)).toBe('boolean');
+    });
+    it('should return true when passed 2', function() {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return true when passed -4', function() {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return true when passed "8"', function() {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return true when passed 0', function() {
+        expect(isEven(0)).toBe(true);
+    });
+    it('should return false when passed the number 3', function() {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when passed a non-numeric string like "banana"', function() {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('should return false when passed the boolean value ' + randomBoolean, function() {
+        expect(isEven(randomBoolean)).toBe(false)
+    });
+});
+
+//isVowel function
+
+
+
